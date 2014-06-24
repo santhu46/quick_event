fa8a8620bc90cf377b947c65702c8094334b6350
Pramod <pramodv_nyros@yahoo.com>
2014-06-24 12:36:40 +0530

    adding users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,27 @@
 class User < ActiveRecord::Base
   has_many :events
+  has_many :friends
   attr_accessible :email, :password, :password_confirmation
   has_secure_password
   validates_presence_of :password, :on => :create
   validates_uniqueness_of :email
+
+
+  def send_friend_request(friend)
+    ## You can override it, but moto is to send a request to another friend
+    ## and update the friends table
+    self.friends << Friend.create(:friend_id => friend.id,:status => "pending") if friend.persisted?
+  end
+
+  def accepting_friend_request(friend)
+    ## check whether the friend request is in pending state for a particular fiend
+    requested_friend =  Friend.where(:friend_id => self.id,:user_id => friend.id)
+    if requested_friend.any? && requested_friend.last.status == "pending"
+      ## Do necessary checking
+      requested_friend.last.status = "true"
+      self.friends << Friend.create(:friend_id => friend.id)
+    end
+
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140619092346) do
+ActiveRecord::Schema.define(:version => 20140620065751) do
 
   create_table "events", :force => true do |t|
     t.integer  "user_id"
@@ -23,6 +23,14 @@ ActiveRecord::Schema.define(:version => 20140619092346) do
   end
 
   add_index "events", ["user_id"], :name => "index_events_on_user_id"
+
+  create_table "friends", :force => true do |t|
+    t.integer  "user_id"
+    t.integer  "friend_id"
+    t.string   "status",     :default => ""
+    t.datetime "created_at",                 :null => false
+    t.datetime "updated_at",                 :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email"
